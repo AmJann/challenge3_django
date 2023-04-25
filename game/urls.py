1d1dd37215f405b1810a2843be0257e3230ce4d2
amjan <amjan91@gmail.com>
2023-04-25 18:33:12 -0400

    completed game app

---
+++ game/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views 
+
+urlpatterns =[
+    path('computer/', views.ComputerListCreate.as_view(), name ='computers'),
+    path('computer_create/', views.ComputerListCreate.as_view(), name='computer_create'),
+    path('computer/<int:id>', views.ComputerViewUpdateDelete.as_view(), name='computer'),
+    path('computer_update/<uuid:pk>/', views.ComputerViewUpdateDelete.as_view(), name='computer_update'),
+    path('computer_delete/<uuid:pk>/', views.ComputerViewUpdateDelete.as_view(), name='computer_delete'),
+    path('player/', views.PlayerListCreate.as_view(), name ='computers'),
+    path('player_create/', views.PlayerListCreate.as_view(), name='computer_create'),
+    path('player/<int:id>', views.PlayerViewUpdateDelete.as_view(), name='computer'),
+    path('player_update/<uuid:pk>/', views.PlayerViewUpdateDelete.as_view(), name='computer_update'),
+    path('player_delete/<uuid:pk>/', views.PlayerViewUpdateDelete.as_view(), name='computer_delete'),
+    path('top-scores/', TopScoresView.as_view(), name='top-scores'),
+
+]
--- game/views.py
+++ game/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import UserSearlizer
+from .models import Player
+from .models import Computer
 
-# Create your views here.
+class ComputerListCreate(generics.ListCreateAPIView):
+    queryset = Computer.objects.all()
+    serializer_class = ComputerSerializer
+
+class ComputerViewUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Computer.objects.all()
+    serializer_class = ComputerSerializer
+
+class PlayerListCreate(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class PlayerViewUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class TopScoresView(APIView):
+    def get(self, request):
+        top_scores = Player.objects.order_by('-score')[:5]
+        serializer = PlayerSerializer(top_scores, many=True)
+        return Response(serializer.data)
+
